351e7aa8f2deef2ffae2d9a3c6b8ee5e5bbd1aca
Kyle Bebak <kylebebak@gmail.com>
2017-05-03 17:10:37 -0500

    Almost done implementing way to specify env_file dynamically

--- http_requests.py
+++ http_requests.py
@@ -1,5 +1,8 @@
 import sublime, sublime_plugin
 
+import os
+import re
+
 import requests
 from requests import delete, get, head, options, patch, post, put
 
@@ -7,6 +10,7 @@ from requests import delete, get, head, options, patch, post, put
 class RequestCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
+        self.config = sublime.load_settings('http_requests.sublime-settings')
         self.import_variables()
         selections = self.get_selections()
         for s in selections:
@@ -14,7 +18,20 @@ class RequestCommand(sublime_plugin.TextCommand):
             print(response.text)
 
     def import_variables(self):
-        with open('/Users/kylebebak/GoogleDrive/Code/Config/ST/Packages/http_requests/_requests_variables.py') as f:
+        requests_file_path = self.view.file_name()
+        requests_file_dir = os.path.dirname( requests_file_path )
+        env_file = self.config.get('env_file') # first get `env_file` from settings
+
+        p = re.compile('\s*env_file\s*=.*') # `env_file` can be overridden from within requests file
+        with open(requests_file_path) as f:
+            for line in f:
+                m = p.match(line)
+                if m:
+                    exec(line, globals(), locals())
+                    break
+
+        env_file_path = os.path.join( requests_file_dir, env_file )
+        with open(env_file_path) as f:
             exec(f.read(), globals())
 
     def get_selections(self):
