1c16c8e98845550b19f6c75db5253805f656c636
Kyle Bebak <kylebebak@gmail.com>
2017-05-03 14:24:52 -0500

    Allow multiple requests to be made at once by iterating over selections

--- http_requests.py
+++ http_requests.py
@@ -8,10 +8,22 @@ class RequestCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
         self.import_variables()
-        request = self.view.substr(self.view.line(self.view.sel()[0]))
-        response = eval(request)
-        print(response.text)
+        selections = self.get_selections()
+        for s in selections:
+            response = eval(s)
+            print(response.text)
 
     def import_variables(self):
-        with open('/Users/kylebebak/GoogleDrive/Code/Config/ST/Packages/http_requests/_request_variables.py') as f:
+        with open('/Users/kylebebak/GoogleDrive/Code/Config/ST/Packages/http_requests/_requests_variables.py') as f:
             exec(f.read(), globals())
+
+    def get_selections(self):
+        view = self.view
+        selections = []
+        for region in view.sel():
+            if not region.empty():
+                selections.append( view.substr(region) )
+            else:
+                selections.append( view.substr(view.line(region)) )
+        return selections
+
