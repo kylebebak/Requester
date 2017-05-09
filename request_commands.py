fca79478761911c69ece595ff1731588d7baf2d7
Kyle Bebak <kylebebak@gmail.com>
2017-05-09 16:10:24 -0500

    Fixed small bug with incorrect name being used for view settings

--- request_commands.py
+++ request_commands.py
@@ -51,7 +51,7 @@ class RequesterCommand(RequestCommandMixin, sublime_plugin.TextCommand):
 
         # this setting allows keymap to target response views separately
         view.settings().set('requester.response_view', True)
-        view.settings().set('requester.requests_file_path', self.view.file_name())
+        view.settings().set('requester.requester_file', self.view.file_name())
         view.settings().set('requester.selection', request)
 
         view.set_name('{}: {}'.format(
